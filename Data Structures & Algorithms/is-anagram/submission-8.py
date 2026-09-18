0c9c471ded5103a219efb74d5e253d0f9998dfc3
Ivan Sostaric <46980578+Ivansost@users.noreply.github.com>
2026-09-18 11:25:56 -0400

    Add: is-anagram - submission-8

---
+++ Data Structures & Algorithms/is-anagram/submission-8.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        dickS = {}
+        dickT = {}
+
+        if len(s) != len(t):
+            return False
+
+        for i in s:
+            dickS[i] = dickS.get(i,0) + 1
+        for j in t:
+            dickT[j] = dickT.get(j,0) + 1
+
+        return dickS == dickT
+
+        
+        
